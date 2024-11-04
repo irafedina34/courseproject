b428359bfd81304f8b98cabad0d8bbc0ba6e687f
QusDev <qusdeveloper@gmail.com>
2024-11-04 14:48:36 +0200

    testimonials

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,13 @@ const our_service_swiper = new Swiper(".our-service__swiper", {
     prevEl: ".our-service__header-swiper-button-prev",
   },
 });
+
+const testimonials_swiper = new Swiper(".testimonials__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 64,
+  loop: true,
+  navigation: {                 
+    nextEl: '.testimonials__swiper-button-next',
+    prevEl: '.testimonials__swiper-button-prev',
+  },
+});
